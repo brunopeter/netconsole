88e01c6ca89f2d79f56bb2629bfb3baead4cfcf6
Peter Bruno <brunopeter@users.noreply.github.com>
2021-02-22 10:20:13 -0500

    Update and rename sessions.sh to launchconsole.sh
    renamed file.  added comments for troubleshooting.

--- launchconsole.sh
+++ launchconsole.sh
@@ -6,3 +6,7 @@ fi
 if dmesg | grep -q ttyUSB0; then
  screen -S byobu -X exec screen -t USB /dev/ttyUSB0 9600,cs8
 fi
+
+# If no sessions open, try looking for a potential tty manually
+# example:  lsusb
+# example:  dmesg | grep tty
